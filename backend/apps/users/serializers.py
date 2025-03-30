5cc45dfab6280b7a61d271dc813170c580a26d88
Николай Екатеринин <eh37@ya.ru>
2025-03-30 23:07:06 +0300

    создание serializers в users

---
+++ backend/apps/users/serializers.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from models import CustomUser
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        validators=[validate_password]
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'username',
+            'first_name',
+            'last_name',
+            'image',
+            'email',
+            'telegram',
+            'telephone',
+            'vk',
+            'ok',
+            'whatsapp',
+            'linkedin',
+            'password'
+        ]
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'image',
+            'email',
+            'telegram',
+            'telephone',
+            'vk',
+            'ok',
+            'whatsapp',
+            'linkedin',
+            'password'
+        ]
+    read_only_fields = ['id']
