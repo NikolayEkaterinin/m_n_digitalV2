ea8c4f6737321ee151802ff0ce03eef287b7564a
Николай Екатеринин <eh37@ya.ru>
2025-04-09 00:20:00 +0300

    Подключение к backend кнопки регистрации при неавторизованном окно профиля при авторизованном.
    сделал выпадающее меню профиля

--- backend/staticfiles/js/app.js
+++ backend/staticfiles/js/app.js
@@ -15,3 +15,18 @@ function setActive(element) {
     document.querySelectorAll('.review_category p').forEach(p => p.classList.remove('active'));
     element.classList.add('active');
 }
+
+
+$(document).ready(function() {
+  $('.dropdown-toggle').click(function(e) {
+    e.preventDefault();
+    $(this).parent().toggleClass('show');
+  });
+
+  // Закрыть меню при клике вне его
+  $(document).click(function(e) {
+    if (!$(e.target).closest('.dropdown').length) {
+      $('.dropdown').removeClass('show');
+    }
+  });
+});
