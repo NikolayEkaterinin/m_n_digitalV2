eb4c2addfdb862dc6373abc29ebcfebd6449517f
Николай Екатеринин <eh37@ya.ru>
2025-04-13 01:26:12 +0300

    дефолтный аватар и завершил работу с формой входа

--- backend/apps/pages/urls.py
+++ backend/apps/pages/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import IndexListView, UserCreateView
+from .views import IndexListView, UserCreateView, UserLoginView
 
 app_name = 'pages'
 
 urlpatterns = [
     path('', IndexListView.as_view(), name='index'),
     path('create/', UserCreateView.as_view(), name='create_user'),
+    path('login/', UserLoginView.as_view(), name='login')
 ]
--- backend/apps/pages/views.py
+++ backend/apps/pages/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.views.generic import (CreateView,
-                                  TemplateView)
-
+                                  TemplateView,
+                                  FormView)
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
 from apps.projects.models import Project, Category
 from apps.users.models import CustomUser
 from .forms import CustomUserForm
@@ -25,6 +28,7 @@ class IndexListView(TemplateView):
         return context
 
 
+# Create user
 class UserCreateView(CreateView):
     model = CustomUser
     form_class = CustomUserForm
@@ -36,3 +40,27 @@ class UserCreateView(CreateView):
         login(self.request,
               self.object)
         return response
+
+
+# Login user
+class UserLoginView(FormView):
+    template_name = 'registration/authorization.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('pages:index') #Пока нет профиля переходим на основную страницу
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect(self.success_url)
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        messages.success(self.request,
+                         f'Добро пожаловать, {user.username}!')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request,
+                       'Ошибка входа. Проверьте логин и пароль')
+        return super().form_invalid(form)
